ebeaf8e69bf46938b8ed44c7ca2220c9127e0287
GuidoA_Amparo <GuidoA_Amparo@jce.do>
2023-08-02 18:39:28 -0400

    fix

---
+++ lib/config/router/app_router_notifier.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/foundation.dart';
+import 'package:teslo_shop/features/auth/presentation/providers/auth_provider.dart';
+
+// class GoRouterNotifier extends ChangeNotifier {
+//   final AuthNotifier
+// }
